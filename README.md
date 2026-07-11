7f3a1db2ec0fd79097c2011b692b5538deab1e7a
SATAKE TAKESHI <satake-takeshi@hotmail.co.jp>
2026-07-11 18:25:03 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Arcane Countdown System
+a fan-made countdown web application built with Python and Streamlit.
+
+## Features
+
+- Countdown to July 16, 2026
+- Animated loading sequence
+- Celebration effects
+- Custom background support
+- Reset function
+
+## Technokogies
+
+- Python 
+- Streamlit
+
+# Author 
+Takenokoko
---
+++ a_app.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import base64
+from datetime import date
+import time
+import os
+
+
+#=============
+#背景設定
+#=============
+def set_background(image_file):
+    with open(image_file, "rb")as f:
+        data= base64.b64encode(f.read()).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp{{
+           background-image:url("data:image/jpg;base64,{data}");
+           background-size: cover;   
+           background-position: right bottom;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+if os.path.exists("images/Arcane_Coundown_kaba-2.jpg"):
+    set_background("images/Arcane_Coundown_kaba-2.jpg")
+
+#=============================
+#アプリのスタイル、見た目の設定
+#=============================
+left, right = st.columns([1,2])
+
+with right:
+    st.title("🚀Arcane Countdown System")
+    st.subheader("Synchronizing with July 16, 2026...")
+    
+    release_date = date(2026,7,16)
+    today = date.today()
+    days_left = (release_date-today).days
+
+#=============
+#ボタンを設置
+#=============
+    if st.button("LAUNCH COUNTDOWN",use_container_width=True):
+        status_text = st.empty()
+        progress_bar = st.progress(0)
+    
+        #演出用カウントダウンループ
+        for i in range(10,0,-1):
+            status_text.markdown(f"### ⚙ Loading resources...{i}")
+            progress_bar.progress((10 - i )* 10)
+            time.sleep(0.3)
+            
+        status_text.empty()
+        progress_bar.empty()
+        
+        #結果表示
+        st.balloons()
+        st.success(f"🎉Synchronized! {days_left} Days remaining until ARCANE!") 
+        
+        #==============
+        #中央画像にする
+        #==============
+        col1,col2,col3 = st.columns([1,2,1])
+
+        with col2:
+            if os.path.exists("images/IMG_7200.png"):
+                st.image("images/IMG_7200.png",width=300)
+
+        #HTML設定
+        st.markdown(
+            """
+        <h2 style="text-align:center; color:white; text-shadow:2px 2px 5px black;">
+           ARCANE is waiting for you!
+        </h2>
+            """,unsafe_allow_html=True)
+        
+        if days_left <= 1:
+            st.fireworks()#残り一日なら花火演出
+    
+        if st.button("EXIT"):
+            st.rerun()
